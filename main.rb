83d069c991dcf968489304528f334d009b8a215c
ivysam-cmyk <98635284+ivysam-cmyk@users.noreply.github.com>
2022-08-25 21:50:49 +0800

    Update main.rb
    wrap from z to a.
    keep the same case.

--- main.rb
+++ main.rb
@@ -1,9 +1,7 @@
-
-def caesor_cipher(phrase,shift_factor = 4 )
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    alphabet = alphabet.map do |each_alphabet|
-        each_alphabet.downcase
-    end
+require 'pry-byebug'
+def caesor_cipher(phrase,shift_factor = 5 )
+    phrase
+    alphabet = ['A','a', 'B','b', 'C','c', 'D','d', 'E','e', 'F','f', 'G','g', 'H','h', 'I','i', 'J','j', 'K','k', 'L','l', 'M','m', 'N','n', 'O','o', 'P','p', 'Q','q', 'R','r', 'S','s', 'T','t', 'U','u', 'V','v', 'W','w', 'X','x', 'Y','y', 'Z','z']
     new_index_array = []
 
     split_array = phrase.split('')
@@ -15,7 +13,7 @@ def caesor_cipher(phrase,shift_factor = 4 )
         else
         alphabet.each_with_index do |indiv_alphabet,index|#check the match against the alphabet
             if indiv_letter == indiv_alphabet
-                initial_pos = ((index +1) - shift_factor) #find the index of alphabet add 1 to find pos in the actual alphabet and account for the shift_factor
+                initial_pos = ((index +1) + (2*shift_factor)) #find the index of alphabet add 1 to find pos in the actual alphabet and account for the shift_factor
                 new_index_array.push initial_pos
         end
             end
@@ -25,7 +23,10 @@ def caesor_cipher(phrase,shift_factor = 4 )
     p new_index_array
     new_alphabet_array =[]
     new_index_array.each do |new_index|
-        if !((0..25).include?(new_index))
+        if (new_index.to_i > 51)
+            new_index = (new_index - 52)#this num is found using trial and error. The number should allow it cycle from z-a accurately
+        end
+        if !((-50..51).include?(new_index))
             new_alphabet_array.push(new_index)
         
         else
@@ -36,5 +37,5 @@ def caesor_cipher(phrase,shift_factor = 4 )
     p new_alphabet
 end
 
-caesor_cipher('hello you!')
+caesor_cipher('What a string!')
 
